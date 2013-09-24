26f10f751ee6d69d91802d94dc3e0a10fcb03239
marlonwang <marlonwang@163.com>
2013-09-24 20:31:05 +0800

    paint line

---
+++ DrawingPanel/DrawingPanel/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace DrawingPanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(12, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(346, 334);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "选择样式，在以下区域画图";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "直线";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 16);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "矩形";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 64);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(59, 16);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "自由画";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(364, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(96, 125);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "图形选择";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "清空画布";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(12, 87);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(47, 16);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "椭圆";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 378);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
+
---
+++ DrawingPanel/DrawingPanel/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DrawingPanel
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen p, p_white;
+        Point p1, p2,po;
+        bool IsDraw = false;// 是否取点
+
+        int nowX, nowY, newX, newY,fixedX,fixedY;//鼠标位置x，y
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+            p = new Pen(Color.Blue, 1);
+            p_white = new Pen(Color.White, 1);
+            //panel1.Invalidate();
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsDraw = false;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsDraw = true;
+
+            p1 = new Point(e.X, e.Y);
+            po = p1; //po 获取上次mousemove后的新点p2
+
+            nowX = e.X;
+            nowY = e.Y;
+
+            //基点
+            fixedX = e.X;
+            fixedY = e.Y;
+         
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            foreach (Control ct in groupBox1.Controls)
+            {
+                RadioButton rb = ct as RadioButton;
+                if (rb.Checked && rb.Text == "直线")
+                {
+                    if (IsDraw == true)
+                    {
+                        p2 = new Point(e.X, e.Y);
+                        
+                        g.DrawLine(p_white, p1, po);//白线重画——清除
+                        g.DrawLine(p, p1, p2);
+                        po = p2;
+                        
+                    }
+                }
+                else if (rb.Checked && rb.Text == "矩形")
+                {
+                    if (IsDraw == true)
+                    {
+                       
+                        newX = e.X;//当前点
+                        newY = e.Y;
+                        g.DrawRectangle(p_white, nowX, nowY, nowX-fixedX, nowY-fixedY);
+                        g.DrawRectangle(p, newX,newY, newX-fixedX, newY-fixedY);
+                        
+                        //上一个点
+                        nowX = newX;
+                        nowY = newY;
+
+                    }
+                }
+                else if (rb.Checked && rb.Text == "自由画")
+                {
+                    if (IsDraw == true)
+                    {
+                        p2 = new Point(e.X, e.Y);
+                        g.DrawLine(p, p1, p2);
+                        p1 = p2;
+                    }
+                }
+                else if (rb.Checked && rb.Text == "椭圆")
+                {
+                    if (IsDraw == true)
+                    {
+                        p2 = new Point(e.X, e.Y);
+                        g.DrawEllipse(p, 80, 30, 100, 60);
+
+                    }
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g = this.panel1.CreateGraphics();
+            g.Clear(Color.White);
+        }  
+    }
+}
+
